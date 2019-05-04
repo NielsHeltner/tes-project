e1efefa11cae33e4eae58f192b2fe352aca1006d
Niels Heltner <niels.heltner@hotmail.com>
2019-05-04 22:43:44 +0200

    added api key fetching

--- build.sbt
+++ build.sbt
@@ -5,3 +5,5 @@ version := "0.1"
 scalaVersion := "2.11.6"
 
 libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.14.0" % "test"
+libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.1"
+libraryDependencies += "io.spray" %%  "spray-json" % "1.3.5"
--- src/test/scala/HesehusApi.scala
+++ src/test/scala/HesehusApi.scala
@@ -1,4 +1,16 @@
+import scalaj.http.Http
+import spray.json._
+
 class HesehusApi {
+
+  val apiKey: String = {
+    val request = Http("http://localhost:5000/api/v1/Auth").param("apiKey", "t1").postData("")
+    val response = request.asString
+    val json = response.body.parseJson.asJsObject
+    val apiKeyWithQuotes = json.fields("accessToken").toString()
+    apiKeyWithQuotes.substring(1, apiKeyWithQuotes.length - 1)
+  }
+  println(apiKey)
 
   def getAmount: String = {
     "do http get"
