a63798d0d89f4c9dae84dfbee47e5f405880166e
Niclas Mølby <niclas@molby.net>
2019-05-09 13:32:09 +0200

    Added API methods for createIndex and getIndices

--- src/test/scala/HesehusApi.scala
+++ src/test/scala/HesehusApi.scala
@@ -1,10 +1,23 @@
 import Request._
-import play.api.libs.json.Json
+import play.api.libs.json.{JsObject, Json}
 
 class HesehusApi {
 
   def getAmount: String = {
     "do http get"
+  }
+
+  def createIndex: (String, Boolean) =  {
+    val request = post("/api/productsearch/v1/Index")
+    val response = request.asString
+    (Json.parse(response.body).as[JsObject].value("id").as[String], response.is2xx)
+  }
+
+  def getIndices: Array[String] = {
+    val request = get("/api/productsearch/v1/Index")
+    val response = request.asString
+
+    Json.parse(response.body).as[Array[JsObject]].map(obj => obj.value("id").as[String])
   }
 
   def getAlias: Array[String] = {
