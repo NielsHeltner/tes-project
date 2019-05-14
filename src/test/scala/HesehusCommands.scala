d2e137a5cf50e2d406ecc0564701289d9dbf97b5
Antonio Lascari <lascaritheman@gmail.com>
2019-05-14 17:42:52 +0200

    Forgot to add bool

--- src/test/scala/HesehusCommands.scala
+++ src/test/scala/HesehusCommands.scala
@@ -275,7 +275,7 @@ object HesehusSpecification extends Commands {
 
     override def nextState(state: State): State = state
 
-    override def preCondition(state: State): Boolean =
+    override def preCondition(state: State): Boolean = true
 
     override def postCondition(state: State, result: Try[Result]): Prop = {
       val success = result.get == 200
